e0bfb1fbbf33280b824be251238146ec5b1be107
Ana Ptskialadze <anukiptskialadze@gmai.com>
2024-01-14 11:33:59 +0100

    Initial Commit

---
+++ AnaCard/AnaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnaCardApp.swift
+//  AnaCard
+//
+//  Created by Ana Ptskialadze on 14.01.24.
+//
+
+import SwiftUI
+
+@main
+struct AnaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnaCard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnaCard
+//
+//  Created by Ana Ptskialadze on 14.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
